86d77a7f34875bc016f721dc29fbd59684d05467
YaSuki9341 <mailadityaraj148@gmail.com>
2024-02-08 16:33:10 +0530

    Add intial file

---
+++ hw.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+// Check whether given no is prime or not
+
+/*int main()
+{
+    int a;
+    cin >> a;
+    int i = 2;
+    while (i < a)
+    {
+        if (a % i == 0)
+        {
+            cout << "Not Prime for "<< i << endl;
+        }
+
+        else
+        {
+            cout << "Prime for " << i << endl;
+        }
+        i++;
+    }
+
+    return 0;
+}*/
+
+// Fahrenheit to celsius
+
+/*int main()
+{
+    float fahrenheit;
+    cin >> fahrenheit;
+    float celsius = (5.0 / 9) * (fahrenheit - 32);
+    cout << "Fahrenheit = " << fahrenheit << "\n"<< "Celsius = " << celsius;
+    return 0;
+}*/
+
+// sum of n even no
+
+/*int main()
+{
+    int n;
+    cin >> n;
+    int i = 0;
+    int sum = 0;
+    while (i <= n)
+    {
+        if (i % 2 == 0)
+        {
+            sum += i;
+        }
+        i++;
+    }
+    i = 1 ;
+    cout << "Sum from " << i << " to " << n  << " is " << sum ;
+}*/
+
+// Check whether the letter is uppercase or lowercase or digit
+
+/*int main()
+{
+    char a;
+    cin >> a;
+    if (a >= 'A' && a <= 'Z')
+    {
+        cout << "Uppercase" << endl;
+    }
+    else if (a >= 'a' && a <= 'z')
+    {
+        cout << "Lowercase" << endl;
+    }
+    else if (a >= '0' && a <= '9')
+    {
+        cout << "Digit" << endl;
+    }
+    return 0;
+}*/
+
+// Print n no
+
+/*int main()
+{
+
+    int n ;
+    cout << "Enter first no " << endl ;
+    cin >> n ;
+    int i ;
+    cout << "Enter last no " << endl ;
+    cin >> i ;
+    cout << "No in correct order ---> " << endl ;
+    while(i <= n)
+    {
+        cout << i << " " ;
+        i++ ;
+    }
+    cout << endl ;
+     n = 0 ;
+     i = 0 ;
+     cout << "Enter last no " ;
+     cin >> n ;
+     cout << "Enter first no " ;
+     cin >> i ;
+
+     cout << "No in reverse order --> " << endl ;
+     while (i >= n)
+     {
+       cout  << i << " " ;
+       i-- ;
+    }
+    return 0 ;
+}*/
+int main(){
+    int a ;
+    int b ;
+    
+}
